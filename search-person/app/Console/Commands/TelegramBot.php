eac817199783484bb0008fe000dda506a122cfd0
Lucas Paiva <lucas.paiva@creativedesign.com.br>
2025-01-30 10:09:29 -0300

    testing APIs

--- search-person/app/Console/Commands/TelegramBot.php
+++ search-person/app/Console/Commands/TelegramBot.php
@@ -21,26 +21,24 @@ class TelegramBot extends Command
 
         // Obtém as atualizações mais recentes
         $updates = $telegram->getUpdates();
+        $update = end($updates);
 
-        foreach ($updates as $update) {
-            $message = $update->getMessage();
-            $chatId = $message->getChat()->getId();
-            $text = $message->getText();
+        $message = $update->getMessage();
+        $chatId = $message->getChat()->id;
+        $text = $message->text;
 
-            // Lógica para processar o comando
-            if ($text === '/start') {
-                $telegram->sendMessage([
-                    'chat_id' => $chatId,
-                    'text' => 'Olá! Envie um CPF, placa de carro ou número de telefone para buscar informações.',
-                ]);
-            } else {
-                // Aqui você processará a busca de dados
-                $response = $this->buscarDados($text);
-                $telegram->sendMessage([
-                    'chat_id' => $chatId,
-                    'text' => $response,
-                ]);
-            }
+        if ($text === '/start') {
+            $telegram->sendMessage([
+                'chat_id' => $chatId,
+                'text' => 'Olá! Envie um CPF, placa de carro ou número de telefone para buscar informações.',
+            ]);
+        } else {
+            // Aqui você processará a busca de dados
+            $response = $this->buscarDados($text);
+            $telegram->sendMessage([
+                'chat_id' => $chatId,
+                'text' => $response,
+            ]);
         }
     }
 
